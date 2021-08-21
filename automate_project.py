9a4ce7fee10cf6b4a70fbf43c315cf4ebc938d3f
MRaff21 <Manfred@Raffelsieper.net>
2021-08-20 20:30:28 -0400

    The script works but the creation of the files is broken at the moment. Whenever it loops it doesn't create a fresh file it writes the entire first test plus the second test and so on till we run out of memory

--- automate_project.py
+++ automate_project.py
@@ -1,6 +1,8 @@
+import json
 import subprocess
 import os
 import random
+import pprint
 
 #
 
@@ -20,23 +22,28 @@ prov = {
     "Newfoundland and Labrador": ["Saint John's"]
 }
 
+count = 0
+
 city_list = ["Whitehorse", "Yellowknife", "Iqaluit", "Victoria", "Edmonton", "Regina", "Winnipeg", "Ottawa",
              "Quebec City", "Fredericton", "Charlottetown", "Halifax", "Saint John's"]
 
-# inital set of questions
-num_questions = len(prov)
+# ------------------------# Helper Methods
 
-# picks number between 1-13
-option = random.randint(0, num_questions - 1)
+# This method here write to a file but its not correct yet. Its currently writing
+# the same file over and over. It should
+def write_to_file():
+    write_to_file.counter += 1
+    subprocess.run(["touch", "/home/manfred/Documents/dumpFolder/Test_" + str(write_to_file.counter) + ".txt"])
+    formatted_text = pprint.pformat(prov)
+    openfile = open("/home/manfred/Documents/dumpFolder/Test_" + str(write_to_file.counter) + ".txt", "a")
+    openfile.write(formatted_text)
+    openfile.close()
+write_to_file.counter = 0
 
-# subtract 1 from the list so next rotation it will be 1-12
-num_questions = num_questions - 1
 
-# --------------- # Puts all the keys in an index so we can pick the index from the random number
-x = list(prov)
-
-d = {}
-
+# This method will populate the lists inside the dictionary...im like 90% sure this can be done
+# better considering the I am having to index my way through the dictionary instead of just looping
+# through the key value pairs. That is probably why it was not working originally (check later use for example)
 
 def populate_list(dictionary, listOfCity, indexOfOption, CityinIndex):
     pick = random.choice(listOfCity)
@@ -50,36 +57,59 @@ def populate_list(dictionary, listOfCity, indexOfOption, CityinIndex):
             dictionary.get(indexOfOption[CityinIndex]).append(pick)
             pick = random.choice(listOfCity)
 
-# loops 13 times 
-for i in range(13):
+def file_creation():
 
-    # This will update the dict -> x is a list and option is the index of that list
 
-    d.update({x[option]: prov.get(x[option])})
-    d.update({x[option]: populate_list(d, city_list, x, option)})
-# We remove the index that has been put into the list so we never get a duplicate.
-    x.pop(option)
+    # Inital set of questions
+    for k in range(1):
+        num_questions = len(prov)
 
-    # This is for the final loop as it will do one last loop after the list is empty and will exit out of the loop
-	# before we hit an error (probably changet his to while loop before submitting)
-    if (len(x)) == 0:
-        break
-    else:
-        # This will update the index and pick another random number between 0 and the lenth of the current list
-        option = random.randint(0, len(x) - 1)
+        # picks number between 1-13
+        option = random.randint(0, num_questions - 1)
 
-x = prov.values()
+        # subtract 1 from the list so next rotation it will be 1-12
+        num_questions = num_questions - 1
 
-for key in prov:
-    x = prov.get(key)
-    random.shuffle(x)
+        # --------------- # Puts all the keys in an index so we can pick the index from the random number
+        x = list(prov)
 
-# ------------- # This will shuffle the dirtionary around list is not done here
-keys = list(prov.keys())
-random.shuffle(keys)
+        d = {}
 
-newDir = dict()
-for key in keys:
-    newDir.update({key: prov[key]})
 
-print(newDir)
+        # loops 13 times
+        for i in range(13):
+
+            # This will update the dict -> x is a list and option is the index of that list
+
+            d.update({x[option]: prov.get(x[option])})
+            d.update({x[option]: populate_list(d, city_list, x, option)})
+            # We remove the index that has been put into the list so we never get a duplicate.
+            x.pop(option)
+
+            # This is for the final loop as it will do one last loop after the list is empty and will exit out of the loop
+            # before we hit an error (probably changet his to while loop before submitting)
+            if (len(x)) == 0:
+                break
+            else:
+                # This will update the index and pick another random number between 0 and the lenth of the current list
+                option = random.randint(0, len(x) - 1)
+
+        x = prov.values()
+
+        for key in prov:
+            x = prov.get(key)
+            random.shuffle(x)
+
+        # ------------- # This will shuffle the dirtionary around list is not done here
+        keys = list(prov.keys())
+        random.shuffle(keys)
+
+        newDir = dict()
+
+        for key in keys:
+            newDir.update({key: prov[key]})
+
+        write_to_file()
+
+
+file_creation()
--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
 import random
-
+import subprocess
+import json
+import pprint
 
 dict = {
 	"Ontario" : ["Ottawa", "more", "here", "fun"],
@@ -21,5 +23,31 @@ for key in dict:
 	
 	random.shuffle(x)
 #	print(x)
+testfile="try"
+count = 1
 
-print(dict.values())
+for i in range(5):
+	subprocess.run(["touch", "/home/manfred/Documents/dumpFolder/hello" + str(count) + ".txt"])
+	count = count + 1
+f = open("hello.txt", "a")
+
+#print(dict.values())
+
+subprocess.run(["touch", "hello.txt"])
+
+
+x = pprint.pformat(dict)
+
+
+print(x)
+
+
+
+f.write(x)
+f.close()
+
+#subprocess.run(['cat', 'hello.txt'])
+
+#f = open ("hello.txt", "r")
+#print(f.read())
+
