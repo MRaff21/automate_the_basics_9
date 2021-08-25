202afe77b19383c7f1049445629725882aedb7de
MRaff21 <Manfred@Raffelsieper.net>
2021-08-25 16:38:37 -0400

    Added last method that will sort out answers based on index. Format the answer key and should be complete

--- automate_project.py
+++ automate_project.py
@@ -23,37 +23,67 @@ prov = {
 city_list = ["Whitehorse", "Yellowknife", "Iqaluit", "Victoria", "Edmonton", "Regina", "Winnipeg", "Ottawa",
              "Quebec City", "Fredericton", "Charlottetown", "Halifax", "Saint John's"]
 
+shuffled_dict = {}
 
-def shuffle(dictionary_shuffle):
-    for key in dictionary_shuffle:
-        lst = dictionary_shuffle.get(key)
+def shuffle():
+    for key in dict:
+        lst = dict.get(key)
         random.shuffle(lst)
 
-    keys = list(dictionary_shuffle.keys())
+    keys = list(dict.keys())
     random.shuffle(keys)
-
-    shuffled_dict = {}
-
     for key in keys:
-        shuffled_dict.update({key: dictionary_shuffle[key]})
+        shuffled_dict.update({key: dict[key]})
 
-    return shuffled_dict
 
 
 global count
 count = 1
 
 
-def write_to_file(anythingHere):
+def write_to_file(dictionary_here):
     global count
+    subprocess.run(["touch", "/home/lab/Documents/dumpFolder/capitalsquiz" + str(count) + ".txt"])
+    subprocess.run(["touch", "/home/lab/Documents/dumpFolder/capitalsquiz_answers" + str(count) + ".txt"])
 
-    subprocess.run(["touch", "/home/lab/Documents/dumpFolder/Test_" + str(count) + ".txt"])
+    quizFile = open(f'/home/lab/Documents/dumpFolder/capitalsquiz{count}.txt', 'w')
+    answerKeyFile = open(f'captialsquiz_answers{count}.txt', 'w')
 
-    openfile = open("/home/lab/Documents/dumpFolder/Test_" + str(count) + ".txt", "a")
-    openfile.write(json.dumps(shuffle(anythingHere)))
-    openfile.close()
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((''*20) + f'Province Capitals Quiz(Form{count})')
+    quizFile.write('\n\n')
+
+
+    shuffle()
+    for question_num in range(3):
+        key, value = list(shuffled_dict.items())[question_num]
+        quizFile.write(f'\n{question_num + 1}: What is the capital of {key}?\n\n')
+        quizFile.write(f'\tA: {value[0]}\n\n\tB: {value[1]}\n\n\tC: {value[2]}\n\n\tD: {value[3]}\n')
+
+    quizFile.close()
+    #openfile = open("/home/lab/Documents/dumpFolder/Test_" + str(count) + ".txt", "a")
+    #openfile.write(json.dumps(shuffle(anythingHere)))
+    #openfile.close()
     count += 1
+# --- Helper method use this to finish off the task
 
+def findAnswer():
+
+	for i in range(13):
+
+		key, value = list(dict.items())[i]
+
+		for j in range(13):
+			skey, svalue = list(prov.items())[j]
+
+			if key == skey:
+
+				for k in range(4):
+					if svalue[0] == value[k]:
+						print("confirm")
+						index = k
+						answer_key[skey] = index
+						break
 
 def populate_list(dict_here):
     for key in dict_here:
@@ -75,7 +105,7 @@ def populate_list(dict_here):
     return dict_here
 
 
-for i in range(13):
+for i in range(3):
     dict = {}
     dict = prov.copy()
     populate_list(dict)
--- testMethod.py
+++ testMethod.py
@@ -2,11 +2,20 @@ import random
 import subprocess
 
 dict = {
-	"Ontario" : "Ottawa",
-	"BC" : "Victoria",
-	"Nova Scotia" : "Halifax",
-	"Alberta" : 'Edmonton',
-	"Answer" : ['Ottawa']
+	"Northwest Territories" : ["Whitehorse", "Fredericton", "Iqaluit", "Yellowknife"],
+	"Nunavut" : ["Yellowknife", "Fredericton", "Iqaluit", "Victoria"],
+	"Yukon" : ["Whitehorse", "Yellowknife", "Iqaluit", "Victoria"],
+    "Manitoba": ["Winnipeg", "Yellowknife", "Iqaluit", "Victoria"],
+    "Ontario": ["Yellowknife", "Iqaluit", "Victoria", "Ottawa"],
+    "Quebec": ["Yellowknife", "Quebec City", "Iqaluit", "Victoria"],
+    "New Brunswick": ["Fredericton", "Yellowknife", "Iqaluit", "Victoria"],
+    "Prince Edward Island": ["Yellowknife", "Iqaluit", "Charlottetown","Victoria"],
+	"Saskatchewan": ["Yellowknife", "Iqaluit", "Victoria" ,"Regina",],
+	"Nova Scotia": ["Halifax", "Yellowknife", "Iqaluit", "Victoria"],
+    "Newfoundland and Labrador": ["Saint John's", "Yellowknife", "Iqaluit", "Victoria"],
+	"Alberta": ["Edmonton", "Yellowknife", "Iqaluit", "Victoria"],
+	"British Comlumbia": ["Victoria", "Yellowknife", "Iqaluit", "Victoria"]
+
 }
 
 
@@ -14,46 +23,75 @@ dict = {
 
 
 
-lists = ["Ottawa", "Vic", "Hali", "Edmonton"]
-pick = random.choice(list)
-check = 0
-while check != 3 :
-	if pick in dict["Answer"] :
-		print("Item was not added because its in the list already")
-		pick = random.choice(lists)
-	else:
-		check = check + 1
-		dict["Answer"].append(pick)
-		pick = random.choice(lists)
+#lists = ["Ottawa", "Vic", "Hali", "Edmonton"]
+#pick = random.choice(list)
+#check = 0
+#while check != 3 :
+#	if pick in dict["Answer"] :
+#		print("Item was not added because its in the list already")
+#		pick = random.choice(lists)
+##		check = check + 1
+#		dict["Answer"].append(pick)
+#		pick = random.choice(lists)
 
-print(dict["Answer"])
+#print(dict["Answer"])
 
 
-prov = { 
-	"Yukon" : ["Whitehorse"], 
-	"Northwest Territories" : ["Yellowknife"],
-	"Nunavut" : ["Iqaluit"], 
-	"British Comlumbia" : ["Victoria"], 
-	"Alberta" : ["Edmonton"],
-	"Saskatchewan" : ["Regina"],
-	"Manitoba" : ["Winnipeg"],
-	"Ontario" : ["Ottawa"],
-	"Quebec" : ["Quebec City"],
-	"New Brunswick" : ["Fredericton"],
-	"Prince Edward Island" : ["Charlottetown"],
-	"Nova Scotia" : ["Halifax"],
-	"Newfoundland and Labrador" : ["Saint John's"]
+prov = {
+    "Yukon": ["Whitehorse"],
+    "Northwest Territories": ["Yellowknife"],
+    "Nunavut": ["Iqaluit"],
+    "British Comlumbia": ["Victoria"],
+    "Alberta": ["Edmonton"],
+    "Saskatchewan": ["Regina"],
+    "Manitoba": ["Winnipeg"],
+    "Ontario": ["Ottawa"],
+    "Quebec": ["Quebec City"],
+    "New Brunswick": ["Fredericton"],
+    "Prince Edward Island": ["Charlottetown"],
+    "Nova Scotia": ["Halifax"],
+    "Newfoundland and Labrador": ["Saint John's"]
 }
 
 keys = list(prov.keys())
 random.shuffle(keys)
 
-ShuffledProv = dict()
+#ShuffledProv = dict()
 
-for key in keys:
-	ShuffledProv.update({key: prov[key]})
+#for key in keys:
+	#ShuffledProv.update({key: prov[key]})
 
 #print("\nDic after shuffling")
 #print(ShuffledProv)
+#key, value = list(prov.items())[1]
+#print(key)
+#print(value[1])
+#subprocess.run("ls" "/home/")
 
-subprocess.run("ls" "/home/")
+answer_key = {}
+
+def findAnswer():
+
+	for i in range(13):
+
+		key, value = list(dict.items())[i]
+
+		for j in range(13):
+			skey, svalue = list(prov.items())[j]
+
+			if key == skey:
+
+				for k in range(4):
+					if svalue[0] == value[k]:
+						print("confirm")
+						index = k
+						answer_key[skey] = index
+						break
+
+	print(answer_key)
+
+
+
+
+
+findAnswer()
